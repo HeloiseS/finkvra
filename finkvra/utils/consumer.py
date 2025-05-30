697a50e9862adca2db6abb62d1eb26d09ae305ec
Heloise Stevance <hfstevance@gmail.com>
2025-05-30 10:21:27 +0100

    added the run_sherlock function - not tested yet

--- finkvra/utils/consumer.py
+++ finkvra/utils/consumer.py
@@ -8,7 +8,9 @@ import pandas as pd
 import numpy as np
 import time
 from datetime import datetime
-
+import lasair
+import os
+import json
 
 def process_alerts(alerts: list) -> pd.DataFrame:
     """Process raw alerts into a cleaned DataFrame."""
@@ -28,6 +30,41 @@ def process_alerts(alerts: list) -> pd.DataFrame:
     ]]
 
 
+def run_sherlock(alert_data:pd.DataFrame):
+    """Run Sherlock on the alert data processed by process_data."""
+    # Load the Sherlock configuration
+    sherlock_config = json.load(open(os.path.join(os.path.dirname(__file__), 'sherlock_config.json')))
+    
+    # Initialize the Sherlock client
+    client = lasair.SherlockClient(sherlock_config['url'], token=sherlock_config['token'])
+    
+    # Process each alert
+    for _, row in alert_data.iterrows():
+        response = client.submit_alert(row.to_dict())
+        print(f"Submitted alert {row['candid']} with response: {response}")
+    # the lasair client will be used for fetching Sherlock data
+    L = lasair.lasair_client(os.environ.get('LASAIR_TOKEN'), 
+                             endpoint='https://lasair-ztf.lsst.ac.uk/api')
+    
+    sherl_class = []
+    sherl_separcsec = []
+
+    for i in range(alert_data.shape[0]):
+        _sherl = L.sherlock_position(alert_data.iloc[i]['ra'], alert_data.iloc[i]['dec'], lite=False)
+        sherl_class.append(_sherl['classifications']['transient_00000'][0])
+        try:
+            sherl_separcsec.append(_sherl['crossmatches'][0]['separationArcsec'])
+        except IndexError:
+            # If orphan will get no match 
+            sherl_separcsec.append(np.nan)
+
+    data_w_sherl = alert_data.join(pd.DataFrame(np.atleast_2d([sherl_class, sherl_separcsec]).T, 
+                                                columns=['sherl_class', 'sep_arcsec']))
+    
+    # remove AGNs and Variable Stars
+    mask = (data_w_sherl['sherl_class'] == 'AGN') | (data_w_sherl['sherl_class'] == 'VS')
+    clean_data = data_w_sherl[~mask]
+    return clean_data
 
 def poll_n_alerts(myconfig, topics, n=10, outidr = '~/Data/FinkZTFStream/') -> None:
     """ Connect to and poll fink servers once.
@@ -54,7 +91,9 @@ def poll_n_alerts(myconfig, topics, n=10, outidr = '~/Data/FinkZTFStream/') -> N
 
     # Analyse output - we just print some values for example
     if not np.all([i is None for i in alerts]):
-        clean_dat = process_alerts(alerts)
+        alert_data = process_alerts(alerts)
+        clean_dat = run_sherlock(alert_data)
+        # if clean_dat is empty, we do not save it but log it
         clean_dat.to_parquet(outidr +f'{prefix}_alerts.parquet')
 
 
