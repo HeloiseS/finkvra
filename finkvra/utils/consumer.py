4ee13270d105be8cd2ce31d50cac0a717cc7f9d5
Heloise Stevance <hfstevance@gmail.com>
2025-05-30 14:12:09 +0100

    added utilities to make features from the data I save from Fink ZTF

--- finkvra/utils/consumer.py
+++ finkvra/utils/consumer.py
@@ -10,7 +10,8 @@ import os
 
 # Setup logger
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s.%(funcName)s: %(message)s")
+logging.basicConfig(level=logging.INFO, 
+                    format="%(asctime)s [%(levelname)s] %(name)s.%(funcName)s: %(message)s")
 
 
 
